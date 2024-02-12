9cb751e2170e5ef058e46b606a1bb196bf165e59
Grace Lemelin <glemelin99@gmail.com>
2024-02-11 19:20:06 -0500

    began working on nav and dashboard

--- src/Kanbas/Navigation/index.tsx
+++ src/Kanbas/Navigation/index.tsx
@@ -1,5 +1,5 @@
 import {Link, useLocation} from "react-router-dom";
-// import "./index.css";
+import "./index.css";
 import {FaTachometerAlt, FaRegUserCircle, FaBook, FaRegCalendarAlt} from "react-icons/fa";
 
 function KanbasNavigation() {
