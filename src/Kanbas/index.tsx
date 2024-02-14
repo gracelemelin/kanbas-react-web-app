fd0aafab11c957ead482079e85fd1554c8b926c6
Grace Lemelin <glemelin99@gmail.com>
2024-02-13 23:16:41 -0500

    worked on course nav

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,18 +1,22 @@
-import { Link } from "react-router-dom";
-import Nav from "../Nav";
+import { Routes, Route, Navigate } from "react-router-dom";
 import KanbasNavigation from "./Navigation";
 import Dashboard from "./Dashboard";
+import Courses from "./Courses";
 
 function Kanbas() {
     return (
         <div className="d-flex">
             <div>
-                <KanbasNavigation/>
+                <KanbasNavigation />
             </div>
-            <div style={{flexGrow: 1}}>
-                <h1>Account</h1>
-                <Dashboard/>
-                <h1>Courses</h1>
+            <div style={{ flexGrow: 1 }}>
+                <Routes>
+                    <Route path="/" element={<Navigate to="Dashboard" />} />
+                    <Route path="Account" element={<h1>Account</h1>} />
+                    <Route path="Dashboard" element={<Dashboard />} />
+                    <Route path="Courses/:courseId/*" element={<Courses/>} />
+                </Routes>
+
             </div>
 
         </div>
