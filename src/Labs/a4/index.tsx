aae740bff5af167c665c05422998ff20d44e2909
Grace Lemelin <glemelin99@gmail.com>
2024-02-23 15:03:54 -0500

    made progress on lab

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -2,6 +2,15 @@ import React from "react";
 import ClickEvent from "./ClickEvent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import ReduxExamples from "./ReduxExamples";
 
 const Assignment4 = () => {
 
@@ -15,6 +24,15 @@ const Assignment4 = () => {
       <ClickEvent/>
       <PassingDataOnEvent/>
       <PassingFunctions theFunction={sayHello}/>
+      <EventObject/>
+      <Counter/>
+      <BooleanStateVariables/>
+      <StringStateVariables/>
+      <DateStateVariable/>
+      <ObjectStateVariable/>
+      <ArrayStateVariable/>
+      <ParentStateComponent/>
+      <ReduxExamples/>
     </>
   );
 };
