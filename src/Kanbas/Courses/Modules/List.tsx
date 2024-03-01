003d84f7cff7c20f5a523fde7f2498bc5741857b
Grace Lemelin <glemelin99@gmail.com>
2024-03-01 11:20:57 -0500

    implemented all basic functionality

--- src/Kanbas/Courses/Modules/List.tsx
+++ src/Kanbas/Courses/Modules/List.tsx
@@ -3,45 +3,28 @@ import "./index.css";
 import { modules } from "../../Database";
 import { FaEllipsisV, FaCheckCircle, FaPlusCircle, FaGlasses } from "react-icons/fa";
 import { useParams } from "react-router";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addModule,
+  deleteModule,
+  updateModule,
+  setModule,
+} from "./modulesReducer";
+import { KanbasState } from "../../store";
+
 
 function ModuleList() {
   const { courseId } = useParams();
 
-  const [moduleList, setModuleList] = useState(modules);
+  const moduleList = useSelector((state: KanbasState) => 
+    state.modulesReducer.modules);
+  const module = useSelector((state: KanbasState) => 
+    state.modulesReducer.module);
+  const dispatch = useDispatch();
 
   const modulesList = modules.filter((module) => module.course === courseId);
 
-  const [module, setModule] = useState({
-    name: "New Module",
-    description: "New Description",
-    course: courseId || "",
-  });
-
   const [selectedModule, setSelectedModule] = useState(modulesList[0]);
-
-  const addModule = (module: any) => {
-    const newModule = { ...module,
-      _id: new Date().getTime().toString() };
-    const newModuleList = [newModule, ...moduleList];
-    setModuleList(newModuleList);
-  };
-
-  const deleteModule = (moduleId: string) => {
-    const newModuleList = moduleList.filter(
-      (module) => module._id !== moduleId);
-      setModuleList(newModuleList);
-  };
-
-  // const updateModule = () => {
-  //   const newModuleList = moduleList.map((m) => {
-  //     if (m._id === module._id) {
-  //       return module;
-  //     } else {
-  //       return m;
-  //     }
-  //   });
-  //   setModuleList(newModuleList);
-  // };
 
   return (
     <div className="ps-1 pe-2 pt-5">
@@ -59,16 +42,14 @@ function ModuleList() {
       <hr />
       <ul className="list-group wd-modules">
         <li className="list-group-item">
-          <button onClick={() => {addModule(module)}}>Add</button>
+          <button onClick={() => dispatch(addModule({ ...module, course: courseId }))}>Add</button>
+          <button onClick={() => dispatch(updateModule(module))}>Update</button>
           <input value={module.name}
-            onChange={(e) => setModule({
-              ...module, name: e.target.value
-            })}
+            onChange={(e) => dispatch(setModule({ ...module, name: e.target.value }))}
           />
           <textarea value={module.description}
-            onChange={(e) => setModule({
-              ...module, description: e.target.value
-            })}
+            onChange={(e) => dispatch(setModule({ ...module, description: e.target.value }))
+          }
           />
         </li>
 
@@ -79,16 +60,20 @@ function ModuleList() {
               key={index}
               className="list-group-item"
               onClick={() => setSelectedModule(module)}>
-                <button
-                onClick={() => deleteModule(module._id)}>
-                  Delete
-                </button>
+              <button
+                onClick={() => dispatch(setModule(module))}>
+                Edit
+              </button>
+              <button
+                onClick={() => dispatch(deleteModule(module._id))}>
+                Delete
+              </button>
               <div>
                 <FaEllipsisV className="me-2" />
                 {module.name}
-                <br/>
+                <br />
                 {module.description}
-                <br/>
+                <br />
                 {module._id}
                 <span className="float-end">
                   <FaCheckCircle className="text-success" />
@@ -98,7 +83,7 @@ function ModuleList() {
               </div>
               {selectedModule._id === module._id && (
                 <ul className="list-group">
-                  {module.lessons?.map((lesson) => (
+                  {module.lessons?.map((lesson: any) => (
                     <li className="list-group-item">
                       <FaEllipsisV className="me-2" />
                       {lesson.name}
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -4,61 +4,66 @@ import Dashboard from "./Dashboard";
 import Courses from "./Courses";
 import { useState } from "react";
 import { courses } from "./Database";
+import store from "./store";
+import { Provider } from "react-redux";
 
 function Kanbas() {
-    const { pathname } = useLocation();
+  const { pathname } = useLocation();
 
-    const [mycourses, setCourses] = useState(courses);
-    const [course, setCourse] = useState({
-      _id: "0", name: "New Course", number: "New Number",
-      startDate: "2023-09-10", endDate: "2023-12-15",
-      image: "reactjs.webp"
-    });
-    const addNewCourse = () => {
-      const newCourse = {
-        ...course,
-        _id: new Date().getTime().toString()
-      };
-      setCourses([...mycourses, { ...course, ...newCourse }]);
+  const [mycourses, setCourses] = useState(courses);
+  const [course, setCourse] = useState({
+    _id: "0", name: "New Course", number: "New Number",
+    startDate: "2023-09-10", endDate: "2023-12-15",
+    image: "reactjs.webp"
+  });
+  const addNewCourse = () => {
+    const newCourse = {
+      ...course,
+      _id: new Date().getTime().toString()
     };
-    const deleteCourse = (courseId: string) => {
-      setCourses(courses.filter((course) => course._id !== courseId));
-    };
-    const updateCourse = () => {
-      setCourses(
-        courses.map((c) => {
-          if (c._id === course._id) {
-            return course;
-          } else {
-            return c;
-          }
-        })
-      );
-    };
-  
-
-    return (
-        <div className="d-flex">
-            <div className={pathname.includes("Home") ? "d-none d-md-block" : ""}>
-                <KanbasNavigation />
-            </div>
-            <div style={{ flexGrow: 1 }}>
-                <Routes>
-                    <Route path="/" element={<Navigate to="Dashboard" />} />
-                    <Route path="Account" element={<h1>Account</h1>} />
-                    <Route path="Dashboard" element={
-                        <Dashboard
-                        mycourses={mycourses}
-                        course={course}
-                        setCourse={setCourse}
-                        addNewCourse={addNewCourse}
-                        deleteCourse={deleteCourse}
-                        updateCourse={updateCourse}/>          
-                    } />
-                    <Route path="Courses/:courseId/*" element={<Courses mycourses={mycourses} />} />
-                </Routes>
-            </div>
-        </div>
+    setCourses([...mycourses, { ...course, ...newCourse }]);
+  };
+  const deleteCourse = (courseId: string) => {
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
+  const updateCourse = () => {
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
     );
+  };
+
+
+  return (
+    <Provider store={store}>
+      <div className="d-flex">
+        <div className={pathname.includes("Home") ? "d-none d-md-block" : ""}>
+          <KanbasNavigation />
+        </div>
+        <div style={{ flexGrow: 1 }}>
+          <Routes>
+            <Route path="/" element={<Navigate to="Dashboard" />} />
+            <Route path="Account" element={<h1>Account</h1>} />
+            <Route path="Dashboard" element={
+              <Dashboard
+                mycourses={mycourses}
+                course={course}
+                setCourse={setCourse}
+                addNewCourse={addNewCourse}
+                deleteCourse={deleteCourse}
+                updateCourse={updateCourse} />
+            } />
+            <Route path="Courses/:courseId/*" element={<Courses mycourses={mycourses} />} />
+          </Routes>
+        </div>
+      </div>
+    </Provider>
+
+  );
 }
 export default Kanbas
