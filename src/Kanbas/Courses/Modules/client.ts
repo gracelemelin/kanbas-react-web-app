a2e9c9e8346be2909fc91a2b84d91e0a54c6730f
Grace Lemelin <glemelin99@gmail.com>
2024-04-16 20:10:40 -0400

    front end url

--- src/Kanbas/Courses/Modules/client.ts
+++ src/Kanbas/Courses/Modules/client.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 axios.defaults.withCredentials = true;
+
 const API_BASE = process.env.REACT_APP_API_BASE;
 const COURSES_API = `${API_BASE}/api/courses`;
 const MODULES_API = `${API_BASE}/api/modules`;
